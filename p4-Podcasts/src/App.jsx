9feb7b13b884da78719cba20ee74b0e30da8abdf
nainascode <nainadugar.official@gmail.com>
2026-06-08 20:55:52 +0530

    feat: google authentication successful

--- p4-Podcasts/src/App.jsx
+++ p4-Podcasts/src/App.jsx
@@ -1,8 +1,44 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { jwtDecode } from 'jwt-decode'
 import './App.css'
 
 function App() {
-  
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [user, setUser] = useState(null);
+    const handleCallBack = (res)=>{
+        const user = jwtDecode(res.credential)
+        setLoggedIn(true);
+        setUser(user);
+    }
+    const handleLogOut= ()=>{
+        setLoggedIn(false)
+        setUser(null)
+    }
+    useEffect(()=>{
+        // global google 
+        google.accounts.id.initialize({
+            //deleted this project from google
+            client_id:"361787205946-oq9dh7issk024ap1jca58p4k0e7tg4p5.apps.googleusercontent.com",
+            callback: handleCallBack,
+        });
+        google.accounts.id.renderButton(
+            document.getElementById("SignIn"),{
+                theme: "outline", size:"large"
+            }
+        )
+    }, [loggedIn])
+    return(
+        <>  
+            {
+                !loggedIn? 
+                <button id='SignIn'></button> : 
+                <div>
+                    hi {user.given_name}!!
+                    <button onClick={handleLogOut}>Log Out</button>
+                </div> 
+            }
+        </>
+    )
 }
 
 export default App
